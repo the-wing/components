5c39279e1b8dba7df5ff8de8297cc348bff4d42d
Rae Farine <rfarine1027@gmail.com>
2018-11-09 13:20:45 -0500

    Export Chip, Text, Image, Box for use

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 // UI
-//export { Box } from './ui/Box/Box';
+export { default as Box } from './ui/Box/Box';
 export { default as Button } from './ui/Button/Button';
 export { default as Card } from './ui/Card/Card';
 export { default as Carousel } from './ui/Carousel/Carousel';
-// export { Chip } from './ui/Chip/Chip';
-// export { Collapsible } from './ui/Collapsible/Collapsible';
-// export { Counter } from './ui/Counter/Counter';
+export { default as Chip } from './ui/Chip/Chip';
+// export { default as Collapsible } from './ui/Collapsible/Collapsible';
+// export { default as Counter } from './ui/Counter/Counter';
 export { default as Drawer } from './ui/Drawer/Drawer';
-// export { DropZone } from './ui/DropZone/DropZone';
+// export { default as DropZone } from './ui/DropZone/DropZone';
 // export { ErrorMessage, FormField, Input, InputGroup, Label, Select, TextArea } from './ui/Forms';
-// export { Icon } from './ui/Icon/Icon';
-// export { Image } from './ui/Image/Image';
-// export { List } from './ui/List/List';
-// export { ListItem } from './ui/ListItem/ListItem';
+// export { default as Icon } from './ui/Icon/Icon';
+export { default as Image } from './ui/Image/Image';
+// export { default as List } from './ui/List/List';
+// export { default as ListItem } from './ui/ListItem/ListItem';
 // export { Circle, TextRow } from './ui/Loading';
-// export { Menu } from './ui/Menu/Menu';
-// export { Section } from './ui/Section/Section';
-// export { SocialIcon } from './ui/SocialIcon/SocialIcon';
-// export { Text } from './ui/Text/Text';
+// export { default as Menu } from './ui/Menu/Menu';
+// export { default as Section } from './ui/Section/Section';
+// export { default as SocialIcon } from './ui/SocialIcon/SocialIcon';
+export { default as Text } from './ui/Text/Text';
 
 // Containers
 export { default as Profile } from './containers/Profile/Profile';
