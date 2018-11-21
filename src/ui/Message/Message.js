c497a4b30f7f8d05a8406b87de1f39b8aa27dd13
Rae Farine <rfarine1027@gmail.com>
2018-11-21 16:31:37 -0500

    [MO-152] Finish tablet and mobile styling minus the little curvy piece of the speech bubble message

--- src/ui/Message/Message.js
+++ src/ui/Message/Message.js
@@ -2,14 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { rem } from 'polished';
+import { queries } from 'breakpoints';
 
 const StyledMessage = styled.div`
   background: ${props => props.theme.colors.potpourri.main};
   padding: ${rem('14px')} ${rem('20px')};
-  border-radius: ${rem('40px')};
+
+  @media ${queries.tablet} {
+    border-radius: ${rem('40px')};
+  }
 `;
 
-const Message = ({ children }) => <StyledMessage>{children}</StyledMessage>;
+const Message = ({ children, className }) => (
+  <StyledMessage className={className}>{children}</StyledMessage>
+);
 
 Message.propTypes = {
   children: PropTypes.oneOf(PropTypes.node, PropTypes.arrayOf(PropTypes.node)).isRequired,
