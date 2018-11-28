f7f177b6e36c2b4c2b198582a4931cc7d6ff9f06
Rae Farine <rfarine1027@gmail.com>
2018-11-28 16:21:25 -0500

    Revert "[MO-152] Add pink curve on mobile Message component"
    This reverts commit a850b349c7bc8f6aae145cf52f9d84e8b0eeedd0.

--- src/ui/Message/Message.js
+++ src/ui/Message/Message.js
@@ -2,33 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { rem } from 'polished';
-import pinkCurve from 'assets/img/pinkCurve.svg';
 
 const StyledMessage = styled.div`
-  position: relative;
   background: ${props => props.theme.colors.potpourri.main};
   padding: ${rem('14px')} ${rem('20px')};
-  margin-top: ${rem('10px')};
-
-  &::before {
-    position: absolute;
-    top: ${rem('-10px')};
-    left: ${rem('15px')};
-    display: block;
-    content: '';
-    width: ${rem('13px')};
-    height: ${rem('10px')};
-    background: url(${pinkCurve}) no-repeat;
-    background-size: cover;
-  }
 
   @media ${props => props.theme.queries.tablet} {
     border-radius: ${rem('40px')};
-    margin-top: 0;
-
-    &::before {
-      display: none;
-    }
   }
 `;
 
