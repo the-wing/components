47ec766e652c3af9fe62eb5306805d41f96f4319
Rae Farine <rfarine1027@gmail.com>
2018-11-28 16:21:25 -0500

    Revert "[MO-152] Finish tablet and mobile styling minus the little curvy piece of the speech bubble message"
    This reverts commit 6b19a8f175046d0d4e1b648fae2a953d98db967a.

--- src/ui/Message/Message.js
+++ src/ui/Message/Message.js
@@ -2,20 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { rem } from 'polished';
-import { queries } from 'breakpoints';
 
 const StyledMessage = styled.div`
   background: ${props => props.theme.colors.potpourri.main};
   padding: ${rem('14px')} ${rem('20px')};
-
-  @media ${queries.tablet} {
-    border-radius: ${rem('40px')};
-  }
+  border-radius: ${rem('40px')};
 `;
 
-const Message = ({ children, className }) => (
-  <StyledMessage className={className}>{children}</StyledMessage>
-);
+const Message = ({ children }) => <StyledMessage>{children}</StyledMessage>;
 
 Message.propTypes = {
   children: PropTypes.oneOf(PropTypes.node, PropTypes.arrayOf(PropTypes.node)).isRequired,
