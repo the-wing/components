ecbd10f6c8c88601024f14446f15709bc1675d79
Rae Farine <rfarine1027@gmail.com>
2018-12-04 16:54:59 -0500

    [MO-510] Fix ListItem styling

--- src/ui/ListItem/ListItem.js
+++ src/ui/ListItem/ListItem.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { rem } from 'polished';
 
-import Box from 'ui/Box/Box';
 import Icon from 'ui/Icon/Icon';
-import Text from 'ui/Text/Text';
 
-export const StyledContent = styled(({ underline, ...rest }) => <Box {...rest} />)`
+const StyledListItem = styled.li`
+  display: flex;
+  flex: 1 1 auto;
+  align-items: center;
+`;
+
+const IconContainer = styled.span`
+  display: inline-block;
+  width: ${rem('40px')};
+`;
+
+export const StyledContent = styled.span`
+  display: flex;
+  flex: 1;
+  padding: ${rem('14px')} 0;
   border-bottom: ${props => (props.underline ? '1px solid rgba(164, 166, 168, 0.3)' : '')};
 
   > a,
@@ -16,21 +29,24 @@ export const StyledContent = styled(({ underline, ...rest }) => <Box {...rest} /
   }
 `;
 
+const Text = styled.span`
+  color: ${props => props.theme.colors.solitude.main};
+  font-size: ${rem('15px')};
+  letter-spacing: 0.2px;
+  line-height: ${rem('20px')};
+`;
+
 const ListItem = ({ className, children, icon, underline }) => (
-  <Box as="li" className={className}>
-    <Box as="span" vAlignContent="center">
-      {icon && (
-        <Box width={40}>
-          <Icon style={{ lineHeight: '30px' }} name={icon} size={15} color="grayChateau" />
-        </Box>
-      )}
-      <StyledContent grow padding={{ vertical: 14 / 16 }} underline={underline}>
-        <Text color="solitude" size={15 / 16} letterSpacing={0.2} lineHeight={20}>
-          {children}
-        </Text>
-      </StyledContent>
-    </Box>
-  </Box>
+  <StyledListItem className={className}>
+    {icon && (
+      <IconContainer>
+        <Icon style={{ lineHeight: '30px' }} name={icon} size={15} color="grayChateau" />
+      </IconContainer>
+    )}
+    <StyledContent underline={underline}>
+      <Text>{children}</Text>
+    </StyledContent>
+  </StyledListItem>
 );
 
 ListItem.propTypes = {
