a5e4ca0b2118ff55a4075ffb0fed22a88e40d5ec
Rae Farine <rfarine1027@gmail.com>
2018-12-05 14:44:34 -0500

    Export all components for use

--- src/index.js
+++ src/index.js
@@ -4,24 +4,23 @@ export { default as Button } from './ui/Button/Button';
 export { default as Card } from './ui/Card/Card';
 export { default as Carousel } from './ui/Carousel/Carousel';
 export { default as Chip } from './ui/Chip/Chip';
-// export { default as Collapsible } from './ui/Collapsible/Collapsible';
-// export { default as Counter } from './ui/Counter/Counter';
+export { default as Collapsible } from './ui/Collapsible/Collapsible';
+export { default as Counter } from './ui/Counter/Counter';
 export { default as Drawer } from './ui/Drawer/Drawer';
-// export { default as DropZone } from './ui/DropZone/DropZone';
-// export { ErrorMessage, FormField, Input, InputGroup, Label, Select, TextArea } from './ui/Forms';
-// export { default as Icon } from './ui/Icon/Icon';
+export { default as DropZone } from './ui/DropZone/DropZone';
+export { ErrorLoading } from './ui/Errors';
+export { ErrorMessage, FormField, Input, InputGroup, Label, Select, TextArea } from './ui/Forms';
+export { default as Icon } from './ui/Icon/Icon';
 export { default as Image } from './ui/Image/Image';
 export { default as List } from './ui/List/List';
 export { default as ListItem, StyledContent } from './ui/ListItem/ListItem';
 export { default as Message } from './ui/Message/Message';
 export { default as Page } from './ui/Page/Page';
-// export { Circle, TextRow } from './ui/Loading';
-// export { default as Menu } from './ui/Menu/Menu';
-// export { default as Section } from './ui/Section/Section';
-// export { default as SocialIcon } from './ui/SocialIcon/SocialIcon';
+export { Circle, TextRow } from './ui/Loading';
+export { default as Menu } from './ui/Menu/Menu';
+export { default as Section } from './ui/Section/Section';
+export { default as SocialIcon } from './ui/SocialIcon/SocialIcon';
 export { default as Text } from './ui/Text/Text';
-
-export { ErrorLoading } from './ui/Errors';
 
 // Containers
 export { default as Member } from './containers/Member/Member';
