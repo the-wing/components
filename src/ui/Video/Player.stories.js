6eb9536ccaedda044545e3c630954ef890da9e53
Ciprian Tanana <ciprian.tanana@gmail.com>
2019-01-11 17:30:37 +0200

    PAMS-1950 package bump + renaming

--- src/ui/Video/Player.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import Player from 'ui/Video/Player';
-
-storiesOf('UI/Video', module)
-  .add('default', () => <Player />)
-  .add('autoplay', () => <Player autoPlay />)
-  .add('muted', () => <Player muted />);
